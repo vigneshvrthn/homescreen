c6f0d8dbc0edb48b123c64fb1752566c50861677
vigneshvrthn <vigneshvrthn@gmail.com>
2026-04-21 21:57:39 +0530

    Save

--- lib/contactuspageweb.dart
+++ lib/contactuspageweb.dart
@@ -15,16 +15,58 @@ class Contactuspageweb extends StatelessWidget {
               child: Container(
                 height: 400,
                 decoration: BoxDecoration(
-                  color: Colors.purple[300],
+                  color: const Color.fromARGB(255, 234, 180, 245),
                   borderRadius: BorderRadius.circular(15),
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset(
-                      "assets/general-manager-illustration-svg-download-png-3239622-removebg-preview.png",
+                    Expanded(
+                      flex: 2,
+                      child: Image.asset(
+                        "assets/general-manager-illustration-svg-download-png-3239622-removebg-preview.png",
+                        height: 250,
+                        width: 250,
+                      ),
                     ),
-                    Text(""),
+                    Expanded(
+                      child: Text(
+                        "General Manager",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ),
+
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.phone, color: Colors.green, size: 30),
+                          SizedBox(width: 15),
+                          Text("82200 42006", style: TextStyle(fontSize: 25)),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            child: Image.asset(
+                              "assets/images/watsapp-removebg-preview.png",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(width: 15),
+                          Text("Chat US!...", style: TextStyle(fontSize: 25)),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
